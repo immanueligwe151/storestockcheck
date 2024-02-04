9e85ab833d23e1d67778578e3ccdec2ff987af55
immanueligwe151 <>
2024-02-04 02:07:58 +0000

    added the functionality to determine how much defrost to put in, as well as done some debugging and editing to other parts of the code

--- run.py
+++ run.py
@@ -13,16 +13,23 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('stockcount')
 
 def return_previous_day_sales():
+    #this function fetches the previous day's sales from the Google worksheet and returns it where required
     worksheet = SHEET.worksheet('sales_sheet')
     data = worksheet.get_all_values()
     return data[len(data) - 1] # retrieves the last line from the sheet, which is the day's previous sales
 
 def append_new_sales(data):
+    #this function adds the current day's sales to the Google worksheet
     worksheet = SHEET.worksheet('sales_sheet')
     worksheet.append_row(data)
     print("The sales for today have been added to the Google sheets for future use")
 
 def amount_input(item, itemtype):
+    '''
+        This is to deal with the input that is to be made the user. As there are several parts of the program where
+        the user input is handled in a similar manner, this function exists to avoid rewriting code to do the exact
+        same thing over and over, and input validation is also included in this
+    '''
     valid = False
     data = 0.0 # set a default value for data
     print()
@@ -34,15 +41,29 @@ def amount_input(item, itemtype):
                 data = input("Number of bags of " + item + " remaining:  ")
             elif (itemtype == "looseleft"):
                 data = input("Number of loose " + item + " remaining:  ")
-            float(data)
+            int(data) 
         except:
             print("Invalid, please enter a numerical value.")
         else:
             valid = True
     return data
 
-def defrost_quantities(previous, sold, bag, loose):
-    print("something")
+def defrost_quantities(previous, sold, bag, loose, held_in_bag):
+    '''
+        This calculates how much product should be brought out for defrost. The program will compare how much was sold
+        the previous day and how much was sold on the current day. Depending on which is more, the program will then
+        determine how much defrost to put in, taking away the defrosted amount on hold from the eventual total.
+    '''
+    new_defrost = 0
+    total_held = (bag * held_in_bag) + loose #the total of the product on hand
+    if (previous > sold):
+        new_defrost = previous + total_held
+        #if more was sold the previous day, we would take the product we have on hand and add it from the total sold yesterday
+    else:
+        new_defrost = sold + total_held + (held_in_bag * 2)
+        #if more was sold today, we would take the number of product sold and add it to what's left, as well as 2 extra bags
+    new_defrost = new_defrost / held_in_bag #converts to bags as it is currently in loose numbers
+    return str(int(new_defrost))
 
 def login():
     print("Welcome manager, please log in to continue:")
@@ -65,6 +86,7 @@ def view_sales():
     print("Zingers: £" + previous_sales[2])
     print("Hot wings: £" + previous_sales[3])
     print("\nTotal sales: £" + str(float(previous_sales[0]) + float(previous_sales[1]) + float(previous_sales[2]) + float(previous_sales[3])))
+    print()
     manager_menu()
 
 def calculate_defrost():
@@ -73,32 +95,36 @@ def calculate_defrost():
     previous_fillets = float(previous_sales[1]) / float(3.0)
     previous_zingers = float(previous_sales[2]) / float(3.0)
     previous_hotwings = float(previous_sales[3]) / float(1.05)
+    #the number of product sold on the previous day
 
     sold_minis = amount_input("mini fillets", "itemsold")
     sold_fillets = amount_input("fillets", "itemsold")
     sold_zingers = amount_input("zingers", "itemsold")
     sold_hotwings = amount_input("hot wings", "itemsold")
 
-    bags_minis = amount_input("mini fillets", "bagsleft")
-    bags_fillets = amount_input("fillets", "bagsleft")
-    bags_zingers = amount_input("zingers", "bagsleft")
-    bags_hotwings = amount_input("hot wings", "bagsleft")
+    bags_minis = int(amount_input("mini fillets", "bagsleft")) * 40
+    bags_fillets = int(amount_input("fillets", "bagsleft")) * 20
+    bags_zingers = int(amount_input("zingers", "bagsleft")) * 20
+    bags_hotwings = int(amount_input("hot wings", "bagsleft")) * 65
+    #multiplying the bags by the number of product in them to add to the total amount of product
 
     loose_minis = amount_input("mini fillets", "looseleft")
     loose_fillets = amount_input("fillets", "looseleft")
     loose_zingers = amount_input("zingers", "looseleft")
     loose_hotwings = amount_input("hot wings", "looseleft")
+    #loose pieces of product that will eventually be added with those in the bags
 
-    sales_minis = str(sold_minis * 1.25)
-    sales_fillets = str(sold_fillets * 3)
-    sales_zingers = str(sold_zingers * 3)
-    sales_hotwings = str(sold_hotwings * 3)
+    sales_minis = str(float(sold_minis) * 1.25)
+    sales_fillets = str(int(sold_fillets) * 3)
+    sales_zingers = str(int(sold_zingers) * 3)
+    sales_hotwings = str(int(sold_hotwings) * 3)
+    #calculates how much sales have been made; not taking in user input of sales so as to avoid error
 
     print()
-    print("\nYou are to defrost " + defrost_quantities(previous_minis, sold_minis, bags_minis, loose_minis) + " bags of mini fillets.")
-    print("\nYou are to defrost " + defrost_quantities(previous_fillets, sold_fillets, bags_fillets, loose_fillets) + " bags of fillets.")
-    print("\nYou are to defrost " + defrost_quantities(previous_zingers, sold_zingers, bags_zingers, loose_zingers) + " bags of zingers.")
-    print("\nYou are to defrost " + defrost_quantities(previous_hotwings, sold_hotwings, bags_hotwings, loose_hotwings) + " bags of minis.")
+    print("\nYou are to defrost " + defrost_quantities(int(previous_minis), int(sold_minis), int(bags_minis), int(loose_minis), 40) + " bags of mini fillets.")
+    print("\nYou are to defrost " + defrost_quantities(int(previous_fillets), int(sold_fillets), int(bags_fillets), int(loose_fillets), 20) + " bags of fillets.")
+    print("\nYou are to defrost " + defrost_quantities(int(previous_zingers), int(sold_zingers), int(bags_zingers), int(loose_zingers), 20) + " bags of zingers.")
+    print("\nYou are to defrost " + defrost_quantities(int(previous_hotwings), int(sold_hotwings), int(bags_hotwings), int(loose_hotwings), 65) + " bags of hot wings.")
 
     append_new_sales([sales_minis, sales_fillets, sales_zingers, sales_hotwings])
     print("\n\n")
@@ -107,7 +133,7 @@ def calculate_defrost():
 
 def manager_menu():
     print("Welcome to the Admin Manager Menu!\nPlease choose from the following options by typing in the relevant number:")
-    print("1. Check yesterday's sales\n2. Calculate defrost for tomorrow")
+    print("1. Check yesterday's sales\n2. Calculate defrost for tomorrow\n3. Quit")
     valid = False
     while(not valid):
         selected_option = input("\nPlease select an option: ")
@@ -117,8 +143,13 @@ def manager_menu():
         elif (selected_option == "2"):
             calculate_defrost()
             valid = True
+        elif (selected_option == "3"):
+            valid = True
+            print("Thank you, bye for now!")
+            quit()
         else:
             print("You've selected an invalid option, please select 1 or 2.")
 
+#this shows the login page first when the program loads
 if (login()):
     manager_menu()
